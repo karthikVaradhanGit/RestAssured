ac796c09e277d6929a0594d40aaf4aa81675e762
karthikVaradhanGit <97653666+karthikVaradhanGit@users.noreply.github.com>
2025-02-10 18:37:22 +0530

    RestAssured

---
+++ src/main/java/RestAssured/RestAssured/Sample.java
@@ -0,0 +1,16 @@
+package RestAssured.RestAssured;
+
+import io.restassured.*;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RestAssured.baseURI = "https://rahulshettyacademy.com/";
+		
+		
+		
+	}
+
+}
